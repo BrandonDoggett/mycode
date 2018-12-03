2ea52ce210f9c59a34ec6d8ca14c464133a0b391
Brandon Doggett <kaydaddy@hotmail.com>
2018-12-03 14:19:44 -0500

    lab 7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Written by: Brandon Doggett
+# Collect IP address info
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:')
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is: " + user_input)
+
+## Collect and store input from the user then print
+user_input2 = input('Please enter the vendor name:')
+print("You told me the vendor name is: " + user_input2)
