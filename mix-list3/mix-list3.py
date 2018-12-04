3dfbd352a0e51f0c7976920e91b206269717c997
Brandon Doggett <kaydaddy@hotmail.com>
2018-12-04 10:36:37 -0500

    Lab 12

---
+++ mix-list3/mix-list3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Written by Brandon Doggett
+# Lab 12 - Lists of lists
+
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['juniper'])
+print(list1)
+list1.append(['10.1.0.1', '10.2.0.1', '10.3.0.1'])
+print(list1)
+print(list1[4])
+print(list1[4][0])
+
+# Challenge 
+animals = ['Fox','Fly','Ant','Bee','Cod','Cat','Dog','Yak','Cow','Hen','Koi','Hog','Jay','Kit']
+print(animals)
