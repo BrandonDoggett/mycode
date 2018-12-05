9070eea7d5bd5463b1e79e9bc1b223c6aff8aded
Brandon Doggett <kaydaddy@hotmail.com>
2018-12-05 11:50:13 -0500

    Lab 20

---
+++ intofor/forloop1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Lab 20 - Looping with for
+# Written by Brandon Doggett
+
+vendors = ['cisco', 'juniper', 'big_ip', 'f5', 'arista']
+
+for x in vendors:
+    print('The vendor is:' + x)
+print('\nOur loop has ended.')
