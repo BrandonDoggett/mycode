b48ef89697a53680c8dfb7a1232fad64fa1695e4
Brandon Doggett <kaydaddy@hotmail.com>
2018-12-06 09:39:33 -0500

    Lab 26

---
+++ smallscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Written by Brandon Doggett
+Lab 26 pylint Code Linting 101
+"""
+
+def main():
+    """Our main program"""
+    mynote = "print this string"
+    print(mynote)
+
+main()
